bdacc3c198d44033323e679022b40cd69424aa91
PipiMelan <1793126995@qq.com>
2020-07-17 12:20:47 +0800

    1.Companion Object

---
+++ src/main/kotlin/CompanionObject(伴生对象).kt
@@ -0,0 +1,11 @@
+
+/*
+* Companion object
+*
+* */
+
+fun main() {
+    Util.URL
+    Util.loadData() // Static method
+    Util().show()   // Instance method
+}
---
+++ src/main/kotlin/Util.java
@@ -0,0 +1,15 @@
+
+
+ public class Util {
+    public static String URL = "www.baidu.com";
+
+    //static method
+    public static void loadData(){
+        System.out.println("Loading pictures");
+    }
+    //instance method
+//We must instantiate the object of this class and use this object to create
+    public void show(){
+
+    }
+}
